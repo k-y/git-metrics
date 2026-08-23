6d1ca1cf023977dc3e0d83a54cdacb2e134cef92
KY <kevikev@gmail.com>
2026-08-22 20:09:15 -0700

    Add reference source for VI

---
+++ 7dtd/VisualIndicator/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyTitle("TechFreqsVisualIndicatorMod(V2.4)")]
+[assembly: AssemblyDescription("TechFreqs Visual Alerts(Visual Indicator Mod), for 7 Days to Die by providing visual indicators on the compass for nearby entities like zombies, passive animals, and hostile animals within a configurable radius")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("TechFreq")]
+[assembly: AssemblyProduct("TechFreqsVisualIndicatorMod(V2.4)")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("7268c152-1904-4486-9ea6-f27c413bc4ce")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
